f2739ce237ba9be1419173fd26a8d37148920c9c
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-10-25 19:24:18 -0300

    styles

---
+++ src/assets/styles/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    primaryBgColor: '#196e2e',
+    secondBgColor: '#E9ECEF',
+    thirdBgColor: '#53f507',
+
+    titleColor: '#f4f5f9',
+
+    textColor: '#565656',
+}
---
+++ src/assets/styles/constants.ts
@@ -0,0 +1,33 @@
+import XboxBackground from '../img/xbox-live-background.jpg'
+import XboxWallpaper from '../img/xbox-wallpaper.png'
+import BackIcon from '../icon/Voltar.svg'
+import coolBackground from '../img/cool-background.png'
+import AddGames from '../icon/add_game.svg'
+import Category from '../icon/category.svg'
+import Standard from '../img/standard.png'
+
+export const constants = {
+    logoFontFamily: 'Roboto, sans-serif',
+    logoFontSize: '2rem',
+    logoLineHeight: '140%',
+
+    subTitleFontFamily: 'Roboto Mono, monospace, sans-serif',
+    subTitleFontSize: '1.6rem',
+    subTitleFontHeight: '140%',
+
+    bodyFontFamily: 'Share Tech Mono, monospace',
+    bodyFontSize: '1rem',
+    bodyLineHeight: '140%',
+
+    xboxBackground: `url(${XboxBackground})`,
+    xboxWallpaper: `url(${XboxWallpaper})`,
+    xboxPlayGameBackground: `url(${Standard})`,
+    backIcon: `url(${BackIcon})`,
+
+    homepageBackground: `url(${coolBackground})`,
+    createGamesIcon: `url(${AddGames})`,
+    createGenresIcon: `url(${Category})`,
+
+    transitionEvents: 'all ease-in-out .6s',
+    transformeScale: 'scale(1.2)'
+}
